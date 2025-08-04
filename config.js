b47952842af38eec475bc856fe45f555c86dee9b
wssharptru <wssharptru@users.noreply.github.com>
2025-08-04 12:59:33 +0000

    deploy: f59a1b97b1583cf8d3387b140e88f69dbe7e6a0d

---
+++ config.js
@@ -0,0 +1,12 @@
+// --- CONFIGURATION & KEYS --- //
+// These objects will be populated by the GitHub Actions workflow
+const firebaseConfig = {
+    apiKey: "__FIREBASE_API_KEY__",
+    authDomain: "__FIREBASE_AUTH_DOMAIN__",
+    projectId: "__FIREBASE_PROJECT_ID__",
+    storageBucket: "__FIREBASE_STORAGE_BUCKET__",
+    messagingSenderId: "__FIREBASE_MESSAGING_SENDER_ID__",
+    appId: "__FIREBASE_APP_ID__",
+    measurementId: "__FIREBASE_MEASUREMENT_ID__"
+};
+const finnhubApiKey = "__FINNHUB_API_KEY__";
