68adb6ff07ff726d47012a8dff3dcfa2c2cf7b2a
wssharptru <162385502+wssharptru@users.noreply.github.com>
2025-08-07 22:05:05 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,7 +147,7 @@ function initializeAuth() {
 function registerServiceWorker() {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/sw.js')
+            navigator.serviceWorker.register('./sw.js')
                 .then(registration => console.log('ServiceWorker registration successful with scope: ', registration.scope))
                 .catch(err => console.log('ServiceWorker registration failed: ', err));
         });
